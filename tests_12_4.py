10c9e916a83a0d3c299c5a290ecac3c31c03c065
Andrey <andrey.mikhaylov.n@gmail.com>
2024-11-16 20:40:25 +0300

    import logger, rt_with_exceptions

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,3 +1,133 @@
+import unittest
+import logging
+from HumanMoveTest.rt_with_exceptions import Runner, Tournament
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        """
+        тест метода walk
+        создаётся объект класса Runner с произвольным именем.
+        Далее вызовите метод walk у этого объекта 10 раз.
+        После чего методом assertEqual сравните distance этого объекта со значением 50.
+        """
+        runner = Runner('name')
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        """
+        тест метода run
+        создаётся объект класса Runner с произвольным именем.
+        Далее вызовите метод run у этого объекта 10 раз.
+        После чего методом assertEqual сравните distance этого объекта со значением 100.
+        """
+        runner = Runner('name')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        """
+        совместный тест методов run и walk
+        test_challenge - метод в котором создаются 2 объекта класса Runner с произвольными именами.
+        Далее 10 раз у объектов вызываются методы run и walk соответственно.
+        Т.к. дистанции должны быть разными, используйте метод assertNotEqual, чтобы убедится в неравенстве результатов.
+        """
+        runner1 = Runner('name1')
+        runner2 = Runner('name1')
+        for _ in range(10):
+            runner1.walk()
+            runner2.run()
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+class TournamentTest(unittest.TestCase):
+    is_frozen = False
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        создаётся атрибут класса all_results.
+        Это словарь в который будут сохраняться результаты всех тестов.
+        """
+        cls.all_results = {}
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def setUp(self):
+        """ создаются 3 объекта Runner """
+        self.runners = [
+            Runner("Усэйн", 10),
+            Runner("Андрей", 9),
+            Runner("Ник", 3),
+        ]
+
+    @classmethod
+    def tearDownClass(cls):
+        """ выводятся all_results по очереди в столбец. """
+        for tournament, results in cls.all_results.items():
+            print(f'{tournament}: {{{', '.join([f'{place}: {name}' for place, name in results.items()])}}}')
+
+    def __test_tournament(self, distance:int, *people:str):
+        participants = [runner for runner in self.runners if runner in people]
+        tournament = Tournament(distance, *participants)
+        # У объекта класса Tournament запускается метод start,
+        # который возвращает словарь в переменную all_results.
+        return tournament.start()
+
+    # методы тестирования забегов, в которых создаётся объект Tournament на дистанцию 90.
+    # Ник всегда должен быть последним.
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_tournament1(self):
+        """ тест tournament для двух участников """
+        results = self.__test_tournament(90, 'Усэйн', 'Ник')
+        self.all_results['1'] = results
+        self.assertTrue(results[max(results)] == 'Ник')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_tournament2(self):
+        """ тест tournament для двух других участников """
+        results = self.__test_tournament(90, 'Андрей', 'Ник')
+        self.all_results['2'] = results
+        self.assertTrue(results[max(results)] == 'Ник')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_tournament3(self):
+        """ тест tournament для трех участников """
+        results = self.__test_tournament(90, 'Усэйн', 'Андрей', 'Ник')
+        self.all_results['3'] = results
+        self.assertTrue(results[max(results)] == 'Ник')
+
+    """
+    В данной задаче, а именно в методе start класса Tournament, допущена логическая ошибка. 
+    В результате его работы бегун с меньшей скоростью может пробежать некоторые дистанции быстрее, 
+    чем бегун с большей. Попробуйте решить эту проблему и обложить дополнительными тестами.
+    """
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_tournament4(self):
+        """ тест исправления ошибки в tournament.start """
+        results = self.__test_tournament(5, 'Усэйн', 'Андрей', 'Ник')
+        self.all_results['4'] = results
+        self.assertTrue(results[len(results)] == 'Ник')
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    unittest.main()
+    """
+    Вывод на консоль:
+    {1: Усэйн, 2: Ник}
+    {1: Андрей, 2: Ник}
+    {1: Андрей, 2: Усэйн, 3: Ник}
+    """
+
+
 """
 2024/01/12 00:00|Домашнее задание по теме "Логирование"
 Если вы решали старую версию задачи, проверка будет производиться по ней.
